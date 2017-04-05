dbe27b83a9f92a6ceff73b6cefb5b88fa26f20dd
Kristi Green <kngreen40@gmail.com>
2017-04-05 11:56:35 -0600

    Work on down/upvote done

--- scripts/idea-box.js
+++ scripts/idea-box.js
@@ -1,68 +1,88 @@
-/**Global Variable**/
+/**GLOBAL VARIABLE**/
 var ideaArray = [];
 
-/**Event Listeners**/
+/**CONSTRUCTOR FUNCTION**/
+function CreateIdea(cardId, title, body, quality) {
+    this.cardId = cardId;
+    this.title = title;
+    this.body = body;
+    this.quality = quality;
+}
+
+/**EVENT LISTENERS**/
 $('#save-btn').on('click', function() {
-	var title = $('#title-input').val();
-	var body = $('#body-input').val();
-	var cardId = Date.now();
-	var quality = "swill";
-	var newIdea = new CreateIdea(cardId, title, body, quality);
-	ideaArray.push(newIdea);
-	addToLocalStorage(ideaArray);
-	ideaCard(ideaArray);
-	clearFields();
+    var title = $('#title-input').val();
+    var body = $('#body-input').val();
+    var cardId = Date.now();
+    var quality = "swill";
+    var newIdea = new CreateIdea(cardId, title, body, quality);
+    ideaArray.push(newIdea);
+    addToLocalStorage(ideaArray);
+    ideaCard(ideaArray);
+    clearFields();
+});
+
+$('#output-area').on('click', '#delete-btn', function() {
+    $('.idea-card').closest('#cardId').remove();
+    // page should not reload
+    // remove idea from localStorage
+});
+
+$('#display-area').on('click', '#upvote', function() {
+    var $rank = $(this).parent().find('#rank');
+    console.log(rank);
+    if ($rank.text() === "swill") {
+        $rank.text('plausible');
+    } else if ($rank.text() === "plausible") {
+        $rank.text('genius');
+    }
+});
+
+$('#display-area').on('click', '#downvote', function() {
+    var $rank = $(this).parent().find('#rank');
+    if ($rank.text() === "genius") {
+        $rank.text('
+            plausible ');
+        }
+        else if ($rank.text() === "plausible") {
+            $#rank.text('swill');
+        }
+    });
+
+  $(window).on('load', function() {
+      retrieveLocalStorage();
+      ideaCard();
   });
 
 
-function CreateIdea(cardId, title, body, quality) {
-	this.cardId = cardId;
-	this.title = title;
-	this.body = body;
-	this.quality = quality;
-}
+  /**FUNCTIONS**/
 
 function ideaCard() {
-	$('#display-area').html('');
-	ideaArray.forEach(function(idea){
-		$('#display-area').append(`<article class="idea-card ${idea.cardId}">
-		<h3 contenteditable="true">${idea.title}</h3>
-		<div id="delete-btn" class="vote"></div>
-		<p class="card-body-text" contenteditable="true">${idea.body}</p>
-		<div id="upvote" class="vote"></div>
-		<div id="downvote" class="vote"></div>
-		<p class="ranking">quality:${idea.quality}</p>
+      $('#display-area').html('');
+      ideaArray.forEach(function(idea) {
+          $('#display-area').append(`
+		<article id="${idea.cardId}" class="idea-card ">
+			<h3 contenteditable="true">${idea.title}</h3>
+			<div id="delete-btn" class="vote"></div>
+			<p class="card-body-text" contenteditable="true">${idea.body}</p>
+			<div id="upvote" class="vote"></div>
+			<div id="downvote" class="vote"></div>
+			<p class="ranking">quality:<span id='rank'>${idea.quality}</span></p>
 		</article>`);
-	});
-};
+		      });
+  };
 
 function clearFields() {
-	$('#title-input, #body-input').val("");
+    $('#title-input, #body-input').val("");
 }
 
 function addToLocalStorage(ideaArray) {
-	localStorage.clear();
-	var stringifiedArray = JSON.stringify(ideaArray);
-	localStorage.setItem('cardId', stringifiedArray);
+    localStorage.clear();
+    var stringifiedArray = JSON.stringify(ideaArray);
+    localStorage.setItem('cardId', stringifiedArray);
 };
 
 function retrieveLocalStorage() {
-	ideaArray = JSON.parse(localStorage.getItem('cardId'));
-	return ideaArray;
+    ideaArray = JSON.parse(localStorage.getItem('cardId'));
+    return ideaArray;
 };
-
-$('#output-area').on('click', '#delete-btn', function() {
-	$('idea-card').closest('.cardId').remove();
-  // page should not reload
-  // remove idea from localStorage
-  });
-
-$(window).on('load', function() {
-	retrieveLocalStorage();
-	ideaCard();
-	console.log('hello');
-});
-
-
-  // to rewrite cards to page on page reload
-  // ideaCard(title, body)
