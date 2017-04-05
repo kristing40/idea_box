5f81822f8bd5340213c4c1e39e8bfd646a4140a7
Kristi Green <kngreen40@gmail.com>
2017-04-05 15:40:44 -0600

    Fix upvote/downvote functionality now working

--- scripts/idea-box.js
+++ scripts/idea-box.js
@@ -22,15 +22,15 @@ $('#save-btn').on('click', function() {
     clearFields();
 });
 
-$('#output-area').on('click', '#delete-btn', function() {
-    $('.idea-card').closest('#cardId').remove();
+$('#display-area').on('click', '#delete-btn', function() {
+    $('.idea-card').remove();
+		console.log($(this));
     // page should not reload
     // remove idea from localStorage
 });
 
 $('#display-area').on('click', '#upvote', function() {
     var $rank = $(this).parent().find('#rank');
-    console.log(rank);
     if ($rank.text() === "swill") {
         $rank.text('plausible');
     } else if ($rank.text() === "plausible") {
@@ -41,35 +41,33 @@ $('#display-area').on('click', '#upvote', function() {
 $('#display-area').on('click', '#downvote', function() {
     var $rank = $(this).parent().find('#rank');
     if ($rank.text() === "genius") {
-        $rank.text('
-            plausible ');
-        }
-        else if ($rank.text() === "plausible") {
-            $#rank.text('swill');
-        }
-    });
+        $rank.text('plausible');
+      } else if ($rank.text() === "plausible") {
+        	$rank.text('swill');
+      }
+});
 
-  $(window).on('load', function() {
+$(window).on('load', function() {
       retrieveLocalStorage();
       ideaCard();
-  });
+});
 
 
   /**FUNCTIONS**/
 
 function ideaCard() {
-      $('#display-area').html('');
-      ideaArray.forEach(function(idea) {
-          $('#display-area').append(`
-		<article id="${idea.cardId}" class="idea-card ">
-			<h3 contenteditable="true">${idea.title}</h3>
-			<div id="delete-btn" class="vote"></div>
-			<p class="card-body-text" contenteditable="true">${idea.body}</p>
-			<div id="upvote" class="vote"></div>
-			<div id="downvote" class="vote"></div>
-			<p class="ranking">quality:<span id='rank'>${idea.quality}</span></p>
-		</article>`);
-		      });
+	  $('#display-area').html('');
+	  ideaArray.forEach(function(idea) {
+	  $('#display-area').append(`
+			<article id="${idea.cardId}" class="idea-card ">
+				<h3 contenteditable="true">${idea.title}</h3>
+				<div id="delete-btn" class="vote"></div>
+				<p class="card-body-text" contenteditable="true">${idea.body}</p>
+				<div id="upvote" class="vote"></div>
+				<div id="downvote" class="vote"></div>
+				<p class="ranking">quality:<span id='rank'>${idea.quality}</span></p>
+			</article>`);
+	  });
   };
 
 function clearFields() {
@@ -86,3 +84,7 @@ function retrieveLocalStorage() {
     ideaArray = JSON.parse(localStorage.getItem('cardId'));
     return ideaArray;
 };
+
+function findIdea () {
+
+}
